c7ac86d10ce3d2e538114850dce1677158203659
miyasajid19 <miyasajid19@gmail.com>
2024-08-26 20:32:32 +0530

    demonstrate the use of friend functions to return the sum of private values

---
+++ week3/Question_4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class second;
+class first
+{
+private:
+    int a;
+public:
+    first() : a(432) {};
+    friend int sum(first, second);
+};
+class second
+{
+private:
+    int b;
+
+public:
+    second() : b(407) {};
+    friend int sum(first, second);
+};
+int sum(first A, second B){
+    return A.a+B.b;
+}
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+first a;
+second b;
+cout<<"the sum of their private values is "<<sum(a,b);
+    return EXIT_SUCCESS;
+}
