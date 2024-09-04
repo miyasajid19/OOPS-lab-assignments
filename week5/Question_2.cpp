2544a0c5d575ec341d1d247f5fd678e3a838dc4d
miyasajid19 <miyasajid19@gmail.com>
2024-09-04 16:29:31 +0530

    add solution for the question no 2

---
+++ week5/Question_2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+class Parent
+{
+protected:
+    int a; // Protected member, accessible in Derived class
+
+private:
+    int b; // Private member, NOT accessible in Derived class
+
+public:
+    int c; // Public member, becomes protected in Derived class due to protected inheritance
+
+    Parent()
+    {
+        this->a = 432;
+        this->b = 407;
+        this->c = a + b; // c is initialized as a + b
+    }
+};
+
+class Derived : protected Parent
+{
+public:
+    void display()
+    {
+        cout << "a: " << this->a << endl; // Accessible (protected in Parent)
+        // cout << this->b << endl;        // Not accessible, would cause an error
+        cout << "c: " << this->c << endl; // Accessible (protected in Derived)
+    }
+
+    void Modify(int a, int c)
+    {
+        this->a = a; // Modify the protected member a
+        this->c = c; // Modify the protected member c
+    }
+};
+
+int main()
+{
+    Derived obj;
+    obj.display(); // Displays initial values of a and c
+
+    // Modify values of a and c using Modify function
+    obj.Modify(407, 432);
+    obj.display(); // Displays modified values of a and c
+
+    return EXIT_SUCCESS;
+}
