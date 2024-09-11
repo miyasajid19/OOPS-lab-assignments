6cd0e517aacbbc93d9f850079d8950d2ccdaef59
miyasajid19 <miyasajid19@gmail.com>
2024-09-11 19:10:43 +0530

    add solution for the sixth question

--- week5/Question_6.cpp
+++ week5/Question_6.cpp
@@ -2,79 +2,63 @@
 #include <string>
 #include <cstdlib>
 using namespace std;
+
 class Book
 {
 protected:
-    string name;
     string title;
+    string author;
     float price;
 
 public:
-    Book(string title, string name, float price) : title(title), name(name), price(price) {}
-    void setTitle(string title)
+    Book(string title, string author, float price) : title(title), author(author), price(price) {}
+
+    void setTitle(string title) { this->title = title; }
+    string getTitle() { return this->title; }
+
+    void setAuthor(string author) { this->author = author; }
+    string getAuthor() { return this->author; }
+
+    void setPrice(float price) { this->price = price; }
+    float getPrice() { return this->price; }
+
+    virtual void Display()
     {
-        this->title = title;
-    }
-    string getTitle()
-    {
-        return this->title;
-    }
-    void setPrice(float price)
-    {
-        this->price = price;
-    }
-    float getPrice()
-    {
-        return this->price;
-    }
-    void setName(string name)
-    {
-        this->name = name;
-    }
-    string getName()
-    {
-        return this->name;
-    }
-    void Display()
-    {
-        cout << "Title :: " << this->title << endl;
-        cout << "Name :: " << this->name << endl;
-        cout << "Price :: " << this->price << endl;
+        cout << "Title: " << this->title << endl;
+        cout << "Author: " << this->author << endl;
+        cout << "Price: $" << this->price << endl;
     }
 };
+
 class TextBook : public Book
 {
 private:
     string subject;
 
 public:
-    TextBook(string title, string name, float price, string subject) : Book(title, name, price), subject(subject) {}
-    void setSubject(string subject)
-    {
-        this->subject = subject;
-    }
-    string getSubject()
-    {
-        return this->subject;
-    }
+    TextBook(string title, string author, float price, string subject) : Book(title, author, price), subject(subject) {}
+
+    void setSubject(string subject) { this->subject = subject; }
+    string getSubject() { return this->subject; }
+
     void Display()
     {
         Book::Display();
-        cout << "Subject :: " << this->subject << endl
+        cout << "Subject: " << this->subject << endl
              << endl;
     }
 };
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    Book book1("Jiraya : The Tale of Gallent", "Master Jiraya", 407.432);
+    Book book1("The Tale of Jiraiya the Gallant", "Jiraiya", 407.43);
+    cout << "General Book Information:" << endl;
     book1.Display();
     cout << endl;
-    TextBook textBook1("Jiraya : The Tale of Gallent", "Master Jiraya", 432.407, "Village Hidden In The Leaf");
+
+    TextBook textBook1("The Tale of Jiraiya the Gallant", "Jiraiya", 432.41, "Ninja History");
+    cout << "TextBook Information:" << endl;
     textBook1.Display();
+
     return EXIT_SUCCESS;
 }
