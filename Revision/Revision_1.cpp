7265babb3a9df9565338b2fb26ef506b85d2d7d4
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-17 13:23:07 +0530

    update

--- Revision/Revision_1.cpp
+++ Revision/Revision_1.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+cout<<"all i want was the love and what i get is the pain";
+exit(1);
+cout<<"the moment had passed and we remained like strangers among the sea of graduates";
 
     return EXIT_SUCCESS;
 }
