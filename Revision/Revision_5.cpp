b98c6134058fc1883dc4557e06f20c6964770eaa
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-18 08:45:41 +0530

    demonstrate the constructions of array of objects

--- Revision/Revision_5.cpp
+++ Revision/Revision_5.cpp
@@ -6,17 +6,50 @@ class X
 public:
     int a;
     int b;
+    X() { cout << "default constructor" << endl; }
     X(int a, int b) : a(a), b(b) { cout << "constructor is called" << endl; }
     void display()
     {
-        cout<<this->a<<"\t"<<this->b<<endl<<endl;
+        cout << this->a << "\t" << this->b << endl
+             << endl;
+    }
+    void set(int a)
+    {
+        this->a = this->b = a;
+    }
+    void set(int a, int b)
+    {
+        this->a = a;
+        this->b = b;
     }
 };
 int main()
 {
-    X objs[]={{432,407},{407,432},{1,2}};
+    X objs[] = {{432, 407}, {407, 432}, {1, 2}};
     objs[0].display();
     objs[1].display();
     objs[2].display();
+    X objs1[5];
+    for (int i = 0; i < 5; i++)
+    {
+        objs1[i].set(i,i+10);
+        objs1[i].display();
+    }
+
+    X* obj=nullptr;
+    cout<<"how many data do you have??";
+    int size;
+    cin>>size;
+    obj=new X[size];
+    for(int i=0;i<size;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        obj[i].set(a,b);
+    }
+    for(int i=0;i<size;i++)
+    {
+        obj[i].display();
+    }
     return EXIT_SUCCESS;
 }
