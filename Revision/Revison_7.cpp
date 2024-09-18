908ec010caf5421266fbf43576dcceed8b56a0a4
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-09-18 09:32:33 +0530

    demonstrate the use of friend function

---
+++ Revision/Revison_7.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class Y;
+class X
+{
+private:
+    int a;
+
+public:
+    X()
+    {
+        this->a = 432;
+    }
+    friend int add(X, Y);
+};
+class Y
+{
+private:
+    int a;
+
+public:
+    Y()
+    {
+        this->a = 407;
+    }
+    friend int add(X, Y);
+};
+int add(X a,Y b)
+{
+    return a.a+b.a;
+}
+int main()
+{
+    X x;
+    Y y;
+cout<<add(x,y);
+    return EXIT_SUCCESS;
+}
