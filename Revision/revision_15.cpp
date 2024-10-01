cd7ae27f2293bae1ddfe0d8c21cfeabd8d90fc23
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-10-01 19:01:15 +0530

    hehe

--- Revision/revision_15.cpp
+++ Revision/revision_15.cpp
@@ -1,1 +1,13 @@
-//will complete later
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    return EXIT_SUCCESS;
+}
