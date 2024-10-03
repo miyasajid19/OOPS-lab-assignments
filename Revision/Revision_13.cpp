033d029b09e6662d5d683f543f3e9fecf5af46ae
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-10-03 18:54:56 +0530

    demonstrate the use of friend function used to access the private value of objects of the same class

--- Revision/Revision_13.cpp
+++ Revision/Revision_13.cpp
@@ -1,13 +1,39 @@
 #include <iostream>
-#include<cstdlib>
+#include <cstdlib>
 using namespace std;
+class X
+{
+    int x;
 
+public:
+    X()
+    {
+        this->x = 432;
+    }
+    void set(int value)
+    {
+        this->x = value;
+    }
+    friend bool isEqual(X, X);
+};
+bool isEqual(X obj1, X obj2)
+{
+    cout << "Private value of obj1 is " << obj1.x << endl;
+    cout << "Private value of obj2 is " << obj2.x << endl;
+    cout << "Are private values are equa?? " << endl;
+    return obj1.x == obj2.x;
+}
 int main()
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
+    // #ifndef ONLINE_JUDGE
+    //     freopen("input.txt", "r", stdin);
+    //     freopen("output.txt", "w", stdout);
+    // #endif
+    X obj1;
+    X obj2;
+    cout << boolalpha;
+    cout << isEqual(obj1, obj2) << endl<< endl;
+    obj1.set(407);
+    cout << isEqual(obj1, obj2) << endl;
     return EXIT_SUCCESS;
 }
