06ddf3402e1c7da57b06da6470aabfc7bdfd32c7
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-10-04 20:11:34 +0530

    updated

--- Revision/BankAccount.cpp
+++ Revision/BankAccount.cpp
@@ -1,70 +1,86 @@
-// Create a class BankAccount with a protected member balance. SavingsAccount should
-// inherit from BankAccount, and CurrentAccount should inherit from SavingsAccount. Another
-// class BankManager should be declared as a friend of CurrentAccount and access its protected
-// members. Implement copy constructors and constant member functions to handle account
-// information.
-// Requirements:
-// 1. Use multilevel inheritance with BankAccount, SavingsAccount, and CurrentAccount.
-// 2. Implement a copy constructor in each class to copy account details.
-// 3. Use constant member functions to display account information.
-// 4. Declare BankManager as a friend of CurrentAccount to access and display balance
-// information.
 #include <iostream>
 #include <cstdlib>
 using namespace std;
-class BankManager;
-class BankAccount
-{
+
+class BankManager; // Forward declaration
+
+class BankAccount {
 protected:
     string accountHolderName;
     double balance;
 
 public:
-    BankAccount(string name, double balance = 0)
-    {
-        this->accountHolderName = name;
-        this->balance = balance;
-    }
-    BankAccount(BankAccount const &other)
-    {
-        this->accountHolderName = other.accountHolderName;
-        this->balance = other.balance;
-    }
-};
-class SavingAccount : public BankAccount
-{
-public:
-    SavingAccount(string name, double balance = 0) : BankAccount(name, balance)
-    {
-    }
-    SavingAccount(SavingAccount const &other) : BankAccount(other) {}
-};
-class CurrnetAccount : public SavingAccount
-{
-public:
-    CurrnetAccount(string name, double balalnce) : SavingAccount(name, balalnce) {}
-    CurrnetAccount(CurrnetAccount const &other) : SavingAccount(other) {}
-    friend BankManager;
-};
-class BankManager
-{
-public:
-    void display(CurrnetAccount const temp)
-    {
-        cout << "Account HolderName :" << temp.accountHolderName << "\tand balance is $" << temp.balance << endl;
-    }
-};
-int main()
-{
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-    CurrnetAccount b("uchiha obito", 432);
-    BankManager manager;
-    manager.display(b);
+    BankAccount(string name, double balance = 0) 
+        : accountHolderName(name), balance(balance) {}
+    
+    BankAccount(const BankAccount &other) 
+        : accountHolderName(other.accountHolderName), balance(other.balance) {}
 
-    CurrnetAccount c(b);
+    // Constant member function to display account info
+    virtual void displayInfo() const {
+        cout << "Account Holder Name: " << accountHolderName 
+             << ", Balance: $" << balance << endl;
+    }
+    
+    // Virtual destructor
+    virtual ~BankAccount() {}
+};
+
+class SavingsAccount : public BankAccount {
+public:
+    SavingsAccount(string name, double balance = 0) 
+        : BankAccount(name, balance) {}
+
+    SavingsAccount(const SavingsAccount &other) 
+        : BankAccount(other) {}
+
+    void displayInfo() const override {
+        cout << "Savings Account - ";
+        BankAccount::displayInfo();
+    }
+};
+
+class CurrentAccount : public SavingsAccount {
+public:
+    CurrentAccount(string name, double balance) 
+        : SavingsAccount(name, balance) {}
+
+    CurrentAccount(const CurrentAccount &other) 
+        : SavingsAccount(other) {}
+
+    friend class BankManager; // Granting access to BankManager
+
+    // Display information using friend function
+    void displayInfo() const override {
+        cout << "Current Account - ";
+        SavingsAccount::displayInfo();
+    }
+};
+
+class BankManager {
+public:
+    void display(const CurrentAccount &account) const {
+        // Direct access to protected members
+        cout << "Account Holder Name: " << account.accountHolderName 
+             << ", Balance: $" << account.balance << endl;
+    }
+};
+
+int main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    CurrentAccount b("Uchiha Obito", 432);
+    BankManager manager;
+
+    manager.display(b);
+    b.displayInfo(); // Using the member function to display account info
+
+    CurrentAccount c(b); // Using copy constructor
     manager.display(c);
+    c.displayInfo(); // Using the member function to display account info
+
     return EXIT_SUCCESS;
 }
