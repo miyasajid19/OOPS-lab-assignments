70288b93993c3758f05a2d69aa970d5f3689e572
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-10-04 11:12:27 +0530

    pointer of the object points to the first data member

--- Revision/Revision_14.cpp
+++ Revision/Revision_14.cpp
@@ -1,13 +1,48 @@
 #include <iostream>
-#include<cstdlib>
+#include <cstdlib>
 using namespace std;
+class X
+{
+    int x;
+    int y;
 
+public:
+    X()
+    {
+        x = 432;
+        y = 407;
+    }
+    void display()
+    {
+
+        cout << "X.x==" << this->x << " and addres is  " << &x << endl;
+        cout << "X.y==" << this->y << " and addres is  " << &y << endl;
+    }
+    int *address()
+    {
+        return &x;
+    }
+};
 int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+    X x1;
+    x1.display();
+    X *ptr;
+    ptr = &x1;
+    ptr->display();
+    cout << "address  of pointer  is :: " << ptr << endl;
+if (reinterpret_cast<void*>(ptr) == reinterpret_cast<void*>(x1.address()))
+{
+    cout << "Pointer to the class points to the first data member of the class." << endl;
+}
+else
+{
+    cout << "Pointer to the class does not point to the first data member of the class." << endl;
+}
 
     return EXIT_SUCCESS;
 }
