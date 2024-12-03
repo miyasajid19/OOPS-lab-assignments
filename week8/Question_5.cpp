96e5e76ef974be8852f75c1b6c35a2c6a9f7175a
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2024-12-03 22:17:00 +0530

    add solution for the fifth question

---
+++ week8/Question_5.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+class solution
+{
+public:
+    template <typename T, typename U>
+    void show(T t, U u)
+    {
+        cout << t << "\t" << u << endl
+             << endl;
+    }
+};
+int main()
+{
+// #ifndef ONLINE_JUDGE
+//     freopen("input.txt", "r", stdin);
+//     freopen("output.txt", "w", stdout);
+// #endif
+    solution soln;
+    soln.show(100, "hello hello");
+    soln.show('k', 1500);
+    soln.show(1.23, 2987);
+    return EXIT_SUCCESS;
+}
